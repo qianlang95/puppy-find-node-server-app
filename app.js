aef7d535a011c468ad6a0537987572d8a281836c
qianlang95 <74698914+qianlang95@users.noreply.github.com>
2023-11-14 15:42:14 -0800

    comment apis

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import Hello from "./hello.js"
 import cors from "cors";
+import Comment from './comment.js';
+
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 
 Hello(app)
+Comment(app)
 
 
 
 
-app.listen(4001)
+
+app.listen(4002)
