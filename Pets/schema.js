0b4db5ad8851e46dbed5799aaade5ce6487426b6
menth0s <marslite@pm.me>
2023-12-04 06:21:45 -0500

    Implemented breedId in schema

--- Pets/schema.js
+++ Pets/schema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     name: {type:String, required: true},
     breed: {type:String, required: true},
+    breedId:{type: Number, required: true},
     type: {type:String, required: true},
     age: {type:Number, required: true},
     location: {type:String, required: true},
