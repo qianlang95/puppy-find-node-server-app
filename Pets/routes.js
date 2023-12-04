519db1bca2019fa5afdd75ca24a65382a5eca7f3
menth0s <marslite@pm.me>
2023-12-04 03:44:13 -0500

    Updated and Fixed Post Detail and routes

--- Pets/routes.js
+++ Pets/routes.js
@@ -10,45 +10,27 @@ function PetRoutes(app){
 
 
 
-    // const updatePost = async (req,res) => {
-    //     const {postId} = req.params;
-    //     const post = req.body;
-    //     const status = await dao.updatePost(postId, post);
-    //     res.json(status);
-
-    // }
-
-    const updatePost = (req, res) => {
-        const postId = req.params.id; // Ensure this matches the named route parameter
+    const updatePost =  async(req, res) => {
+        const postId = req.params.id; 
         const post = req.body;
     
-        dao.updatePost(postId, post)
-            .then(status => {
-                console.log('Update status:', status);
-                res.json(status);
-            })
-            .catch(error => {
-                console.error('Update error:', error);
-                res.status(500).json({ error: error.message });
-            });
+        // dao.updatePost(postId, post)
+        //     .then(status => {
+        //         console.log('Update status:', status);
+        //         res.json(status);
+        //     })
+        //     .catch(error => {
+        //         console.error('Update error:', error);
+        //         res.status(500).json({ error: error.message });
+        //     });
+
+        //To visualize the updated post by reading the whole status content
+        const status = await  dao.updatePost(postId, post).then(status => {console.log(`Update Status: ${status}`); res.json(status)  })
+       
     };
 
 
-    // app.put("/api/pets/:id", (req,res) => {
-    //     const {id} = req.params;
-    //     const pet = req.body;
-    //     console.log("Checking if the update works")
-    //     Database.pets = Database.pets.map( (p) => p._id === id ? {...pet} : p );
-    //     res.sendStatus(204)
-    // } )
 
-    // const checkingiD = async (req,res) => {
-    //     // const postId = req.params;
-    //     console.log("Check Here -> ",req.params);
-    //     res.json(req.params)
-    //     // const postData = req.body;
-    //     // console.log(`postId in the router translate to ${postId}`);
-    // }
 
     const deletePost = async (req,res) => {
         const status = await dao.deletePost(req.params.postId);
@@ -57,22 +39,16 @@ function PetRoutes(app){
     }
 
 
-    // app.delete("/api/pets/:id", (req,res) => {
-    //     const {id} = req.params;
-    //     Database.pets = Database.pets.filter( (p) => p._id !== id  );
-    //     res.sendStatus(204)
-    // } )
-
-
-    // app.post("/api/pets", (req,res) => {
-    //     const pet = {...req.body, _id: new Date().getTime().toString() }
-    //     Database.pets.push(pet)
-    //     res.send(pet);
-    // } )
 
     const findAllPosts = async (req,res) =>{
         const posts = await dao.findAllPosts();
         res.json(posts);
+    }
+
+    const findPostById = async (req,res) => {
+        console.log(`Logging post with ID ${req.params.postId}`)
+        const post = await dao.findPostById(req.params.postId);
+        res.json(post);
     }
 
 
@@ -81,6 +57,8 @@ function PetRoutes(app){
     app.put("/api/pets/:id", updatePost);
     app.delete("/api/pets/:postId", deletePost)
     app.get("/api/pets", findAllPosts);
+    app.get("/api/posts/:postId", findPostById);
+
 
 
 
--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(express.json());
 app.use(cors({
-    origin : "http://localhost:3001"
+    origin : "http://localhost:3000"
 }
 ));
 
